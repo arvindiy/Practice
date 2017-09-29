6786046845b869190e24ae664e87bdcc63e9ead8
Arvind <Arvind.Iyer@sephora.com>
2017-09-29 14:17:05 -0700

    Changed the array partition problem's solution

--- Algorithms/src/leet/ArrayPartition561.java
+++ Algorithms/src/leet/ArrayPartition561.java
@@ -18,7 +18,11 @@ class Solution {
 
     public int arrayPairSum(int[] nums) {
         Arrays.sort(nums);
-        return 0;
+        int sum = 0;
+        for(int i = 0 ; i < nums.length-1; i= i+2){
+            sum = sum+ Math.min(nums[i],nums[i+1]);
+        }
+        return sum;
     }
 }
 
