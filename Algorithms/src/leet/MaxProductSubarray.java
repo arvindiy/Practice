9238f3adb46b9cf136af56065746981f85d1e2e2
Arvind <Arvind.Iyer@sephora.com>
2017-10-03 12:16:15 -0700

    Changing the name of the class.

--- Algorithms/src/leet/MaxProductSubarray.java
+++ Algorithms/src/leet/MaxProductSubarray.java
@@ -1,8 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package leet;
 
 /**
  *
- * @author akrishnaniyer Find the contiguous array with the maximum product
+ * @author akrishnaniyer
  */
 public class MaxProductSubarray {
 
@@ -10,13 +15,8 @@ public class MaxProductSubarray {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
-        int arr[] = {6, -3, -10, 0, 2};
-        //int arr[] = {-1, -3, -21, 0, 60};
-        //int arr[] = {-2, -3, 0, -2, -40};
-        //int arr[] = {-2, -1, 3, 4, -1, -2, -1, -5, -4};        
-
-        int prod = maxProdSubarray(arr);
+        int nums[] = {-2, -1, 3, 4, -1, -2, -1, -5, -4};
+        int prod = maxProdSubarray(nums);
         System.out.println(prod);
     }
 
@@ -24,24 +24,13 @@ public class MaxProductSubarray {
         if (nums.length == 0) {
             return 0;
         }
-        int p = nums[0];
-        
-        for (int i = 1, max_so_far = p, min_so_far = p; i < nums.length; i++) {
+        int min_prod, max_prod = 0;
+        int max_so_far = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
             
-            //x = x ^ y ^ (y = x); Integers
-            //a += (b - (b = a)); Doubles and Floats
-
-            if (nums[i] < 0) {
-                int tmp = max_so_far;
-                max_so_far = min_so_far;
-                min_so_far = tmp;
-            }
-
-            max_so_far = Math.max(nums[i], nums[i] * max_so_far);
-            min_so_far = Math.min(nums[i], nums[i] * min_so_far);
-
-            p = Math.max(max_so_far, p);
         }
-        return p;
+        return max_so_far;
     }
+
 }
