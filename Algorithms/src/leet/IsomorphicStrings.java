e52eada2cf30b9109f09240cc0972e0b9b43a132
sahana <sahana@sana>
2017-10-15 19:39:03 -0700

    https://leetcode.com/problems/isomorphic-strings/description/

---
+++ Algorithms/src/leet/IsomorphicStrings.java
@@ -0,0 +1,42 @@
+package leet;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/isomorphic-strings/description/
+
+public class IsomorphicStrings {
+
+	public static void main(String[] args) {
+		String s1 = "ab";
+		String s2 = "ca";
+		System.out.println(s1 + " and " + s2 + " " + isIsomorphic(s1, s2));
+
+	}
+
+	public static boolean isIsomorphic(String a, String b) {
+
+		if (a != null && b != null) {
+			if (a.length() == b.length()) {
+				HashMap<Character, Character> map = new HashMap<Character, Character>();
+				for (int i = 0; i < a.length(); i++) {
+					char c1 = a.charAt(i);
+					char c2 = b.charAt(i);
+					if(map.containsKey(c1)) {
+						if(map.get(c1) != c2)
+							return false;
+					}else {
+						if(map.containsKey(c2)) {
+							return false;
+						}
+						map.put(c1, c2);
+					}
+				}
+			} else
+				return false;
+		} else
+			return false;
+
+		return true;
+	}
+
+}
