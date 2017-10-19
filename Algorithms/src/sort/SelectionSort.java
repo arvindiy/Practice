204a92a5fd386642b3627ef5ca9c0ee80fee1ebe
Arvind <Arvind.Iyer@sephora.com>
2017-10-18 22:22:48 -0700

    Selection Sort

--- Algorithms/src/sort/SelectionSort.java
+++ Algorithms/src/sort/SelectionSort.java
@@ -12,20 +12,21 @@ public class SelectionSort {
 		for (int i = 0; i < nums.length; i++) {
 			System.out.println(nums[i]);
 		}
-		
+
 	}
 
 	private static void doSelectionSort(int nums[]) {
 		int n = nums.length;
 		for (int i = 0; i < n; i++) {
-			int min = nums[i];
-			for (int j = i+1; j < n; j++) {
-				if (nums[j] < min) {
-					min = nums[j];
+			int min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (nums[j] < nums[min]) {
+					min = j;
 				}
 			}
-			nums[i] = min;
+			int tmp = nums[i];
+			nums[i] = nums[min];
+			nums[min] = tmp;
 		}
 	}
-
 }
