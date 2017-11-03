7ba43a1c714767ad3f1664da141fcf2c14bfd6a8
Arvind <Arvind.Iyer@sephora.com>
2017-11-03 11:05:37 -0700

    Binary Tree construction from inorder and preorder traversals

--- Algorithms/src/leet/Binary_Tree_Construction_From_Pre_Order_And_In_Order.java
+++ Algorithms/src/leet/Binary_Tree_Construction_From_Pre_Order_And_In_Order.java
@@ -9,26 +9,21 @@ public class Binary_Tree_Construction_From_Pre_Order_And_In_Order {
 		int[] preOrder = { 1, 2, 4, 5, 3 };
 		int[] inOrder = { 4, 2, 5, 1, 3 };
 		TreeNode root = new Binary_Tree_Construction_From_Pre_Order_And_In_Order().buildTree(preOrder, inOrder);
-
 	}
 
 	public TreeNode buildTree(int[] preorder, int[] inorder) {
-
 		if (preorder.length != inorder.length)
 			return null; // Pre order and inorder are of different trees, if the lengths are not the
 							// same;
-
 		return buildTree(preorder, 0, preorder.length - 1, inorder, 0, inorder.length);
 	}
 
 	private TreeNode buildTree(int[] preOrder, int preStart, int preEnd, int[] inOrder, int inStart, int inEnd) {
-		
 		int value = preOrder[preStart];
-
 		TreeNode node = new TreeNode(value);
 		int index = -1;
 		for (int i = 0; i < inOrder.length; i++) {
-			if(inOrder[i] == value) {
+			if (inOrder[i] == value) {
 				index = i;
 				break;
 			}
