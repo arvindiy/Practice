c1b8c8a82ad021fe2c3d6f933c7b1c2f909fc69c
Arvind <Arvind.Iyer@sephora.com>
2017-11-07 12:15:27 -0800

    Final Changes to serialize and deserialze a BT

--- Algorithms/src/leet/problem_297/Serialize_Deserialize_BT.java
+++ Algorithms/src/leet/problem_297/Serialize_Deserialize_BT.java
@@ -1,5 +1,6 @@
 package leet.problem_297;
 
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 import leet.node.TreeNode;
@@ -27,79 +28,45 @@ public class Serialize_Deserialize_BT {
 		System.out.println(r.val);
 	}
 
+	// Encodes a tree to a single string.
 	public String serialize(TreeNode root) {
-		StringBuilder result = new StringBuilder();
-
-		String s = inOrder(root, new StringBuilder());
-		result.append(s == null ? "" : s);
-		result.append("|");
-		s = preOrder(root, new StringBuilder());
-		result.append(s == null ? "" : s);
-
-		return result.toString();
+		if (root == null)
+			return null;
+		Stack<TreeNode> stack = new Stack<TreeNode>();
+		stack.push(root);
+		StringBuilder sb = new StringBuilder();
+		while (!stack.isEmpty()) {
+			TreeNode h = stack.pop();
+			if (h != null) {
+				sb.append(h.val + ",");
+				stack.push(h.right);
+				stack.push(h.left);
+			} else {
+				sb.append("#,");
+			}
+		}
+		return sb.toString().substring(0, sb.length() - 1);
 	}
 
+	// Decodes your encoded data to tree.
 	public TreeNode deserialize(String data) {
-		TreeNode root = null;
-		if (data == null) {
-			return root;
-		}
+		if (data == null)
+			return null;
+		int[] t = { 0 };
+		String[] arr = data.split(",");
+		return helper(arr, t);
+	}
 
-		StringTokenizer tokenizer = new StringTokenizer(data, "|");
-		int[] preOrder = null;
-		int[] inOrder = null;
-		if (tokenizer.countTokens() == 2) {
-			String inOrder_Str = tokenizer.nextToken();
-			String preOrder_Str = tokenizer.nextToken();
-			StringTokenizer inTokenizer = new StringTokenizer(inOrder_Str, ",");
-			StringTokenizer preTokenizer = new StringTokenizer(preOrder_Str, ",");
-			inOrder = new int[inTokenizer.countTokens()];
-			preOrder = new int[preTokenizer.countTokens()];
-			for (int i = 0; i < inOrder.length; i++) {
-				preOrder[i] = Integer.parseInt(preTokenizer.nextToken());
-				inOrder[i] = Integer.parseInt(inTokenizer.nextToken());
-			}
-			root = buildTree(preOrder, 0, preOrder.length - 1, inOrder, 0, inOrder.length - 1);
+	public TreeNode helper(String[] arr, int[] t) {
+		if (arr[t[0]].equals("#")) {
+			return null;
 		}
+		TreeNode root = new TreeNode(Integer.parseInt(arr[t[0]]));
+		t[0] = t[0] + 1;
+		root.left = helper(arr, t);
+		t[0] = t[0] + 1;
+		root.right = helper(arr, t);
 		return root;
-	}
-
-	private TreeNode buildTree(int[] preOrder, int preStart, int preEnd, int[] inOrder, int inStart, int inEnd) {
-		if (preEnd < preStart || inEnd < inStart)
-			return null;
-
-		int value = preOrder[preStart];
-		int index = -1;
-		for (int i = 0; i < inOrder.length; i++) {
-			if (value == inOrder[i]) {
-				index = i;
-				break;
-			}
-		}
-
-		TreeNode node = new TreeNode(value);
-
-		node.left = buildTree(preOrder, preStart + 1, preStart + (index - inStart), inOrder, inStart, index - 1);
-		node.right = buildTree(preOrder, preStart + (index - inStart) + 1, preEnd, inOrder, index + 1, inEnd);
-		return node;
-	}
-
-	private String inOrder(TreeNode node, StringBuilder result) {
-		if (node == null)
-			return null;
-		inOrder(node.left, result);
-		result.append(result.toString().length() == 0 ? "" : ",").append(node.val);
-		inOrder(node.right, result);
-		return result.toString();
-	}
-
-	private String preOrder(TreeNode node, StringBuilder result) {
-		if (node == null)
-			return null;
-		result.append(result.toString().length() == 0 ? "" : ",").append(node.val);
-		inOrder(node.left, result);
-		inOrder(node.right, result);
-		return result.toString();
 	}
 
 }
