6c86be3ac58bccb9f296a6d97a719d803851dcc7
Arvind <Arvind.Iyer@sephora.com>
2017-11-08 12:23:00 -0800

    Check

--- Algorithms/src/leet/problem_3/LongestSubstring.java
+++ Algorithms/src/leet/problem_3/LongestSubstring.java
@@ -8,18 +8,19 @@ public class LongestSubstring {
 		if (s == null || s.length() == 0)
 			return 0;
 		HashSet<Character> set = new HashSet<Character>();
-		char[] chars = s.toCharArray();
+	
 		int max = 0;
 		int start = 0;
-		for (int i = 0; i < chars.length; i++) {
-			char c = chars[i];
+		for (int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
 			if (!set.contains(c)) {
 				set.add(c);
 				max = Math.max(max, i - start + 1);
 			} else {
 				for (int j = start; j < i; j++) {
-					set.remove(chars[j]);
-					if (chars[j] == c) {
+					char c1 = s.charAt(j);
+					set.remove(c1);
+					if (c == c1) {
 						start = j + 1;
 						break;
 					}
