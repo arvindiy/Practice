52172975d99e69968d698bd00556969b776956ec
Arvind <Arvind.Iyer@sephora.com>
2017-11-09 11:09:59 -0800

    Ugly Number

---
+++ Algorithms/src/leet/problem_263/UglyNumber.java
@@ -0,0 +1,31 @@
+package leet.problem_263;
+
+//https://leetcode.com/problems/ugly-number/description/
+
+public class UglyNumber {
+
+	public static void main(String[] args) {
+		new UglyNumber().isUgly(2);
+	}
+
+	public boolean isUgly(int num) {
+		if (num == 0)
+			return false;
+		if (num == 1)
+			return true;
+		if (num % 2 == 0) {
+			num = num / 2;
+			return isUgly(num);
+		}
+		if (num % 3 == 0) {
+			num = num / 3;
+			return isUgly(num);
+		}
+		if (num % 5 == 0) {
+			num = num / 3;
+			return isUgly(num);
+		}
+		return false;
+	}
+
+}
