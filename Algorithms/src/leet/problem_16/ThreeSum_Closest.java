ff8b12c9ed030714ea99bc41b511bb2e74ebef1d
Arvind <Arvind.Iyer@sephora.com>
2017-11-15 17:42:21 -0800

    THree sum problem

---
+++ Algorithms/src/leet/problem_16/ThreeSum_Closest.java
@@ -0,0 +1,38 @@
+package leet.problem_16;
+
+import java.util.Arrays;
+
+//https://leetcode.com/problems/3sum-closest/description/
+
+public class ThreeSum_Closest {
+
+	public static void main(String[] args) {
+		int[] nums = { -1, 2, 1, -4 };
+		System.out.println(new ThreeSum_Closest().threeSumClosest(nums, 1));
+	}
+
+	public int threeSumClosest(int[] nums, int target) {
+		int min = Integer.MIN_VALUE;
+		int result = 0;
+		Arrays.sort(nums);
+
+		for (int i = 0; i < nums.length; i++) {
+			int j = i + 1;
+			int k = nums.length - 1;
+			while (j < k) {
+				int sum = nums[i] + nums[j] + nums[k];
+				int diff = Math.abs(sum - target);
+				if (diff < min) {
+					min = diff;
+					result = sum;
+				}
+				if (sum <= target)
+					j++;
+				else
+					k--;
+			}
+		}
+		return result;
+	}
+
+}
