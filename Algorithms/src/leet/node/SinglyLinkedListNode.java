664e05febfe337cf045e7db3e11f061f8f2012ce
Arvind <Arvind.Iyer@sephora.com>
2017-12-01 13:09:26 -0800

    Singly Linked List Node

---
+++ Algorithms/src/leet/node/SinglyLinkedListNode.java
@@ -0,0 +1,14 @@
+package leet.node;
+
+/**
+ * This is a node in a singly linked list
+ * @author akrishnaniyer
+ *
+ */
+public class SinglyLinkedListNode {
+	public int val;
+	public SinglyLinkedListNode next;
+	public SinglyLinkedListNode(int val){
+		this.val = val;
+	}
+}
