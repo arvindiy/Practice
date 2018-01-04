ef0573244d4187c3d1d3bf299215342e9068cb7a
Arvind <Arvind.Iyer@sephora.com>
2018-01-04 09:48:40 -0800

    solution to pascal's triangle

---
+++ Algorithms/src/leet/problem_118/PascalsTriangle.java
@@ -0,0 +1,32 @@
+package leet.problem_118;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PascalsTriangle {
+	public List<List<Integer>> generate(int numRows) {
+		ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+		if (numRows <= 0)
+			return result;
+		ArrayList<Integer> pre = new ArrayList<Integer>();
+		pre.add(1);
+		result.add(pre);
+		for (int i = 2; i <= numRows; i++) {
+			ArrayList<Integer> cur = new ArrayList<Integer>();
+			cur.add(1); // first
+			for (int j = 0; j < pre.size() - 1; j++) {
+				cur.add(pre.get(j) + pre.get(j + 1)); // middle
+			}
+			cur.add(1);// last
+			result.add(cur);
+			pre = cur;
+		}
+		return result;
+	}
+
+	public static void main(String ar[]) {
+		System.out.println(new PascalsTriangle().generate(5));
+
+	}
+
+}
