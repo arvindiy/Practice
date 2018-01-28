636ac9ce921b845333b4230f23e592307bf2dfe7
Arvind <Arvind.Iyer@sephora.com>
2018-01-28 13:39:07 -0800

    Added comments

--- Algorithms/src/leet/problem_50/Power.java
+++ Algorithms/src/leet/problem_50/Power.java
@@ -4,16 +4,26 @@ package leet.problem_50;
 public class Power {
 
 	public static void main(String[] args) {
+		//This should return 5 * 5 * 5
 		System.out.println(new Power().myPow(5, 3));
 	}
 
 	public double myPow(double x, int n) {
+		//If n == 0, then any 'x' would result in a '1'
 		if(n == 0) return 1;
+		
+		//If x is 0 or 1, then the result would be x
 		if (x == 0 || x == 1) return x;
+		
+		//If n < 0, then we inverse x, and negate n.
 		if(n < 0)		{
 			x = 1/x;
 			n = n * ( -1);
 		}
+		
+		//If n == MIN value, 
+		//and x > 0, any x would result in a 1.
+		//If x < 0, any x would result in a 0.
 		if(n == Integer.MIN_VALUE) {
 			if(x>0) {
 				return 1;				
@@ -21,6 +31,8 @@ public class Power {
 				return 0;
 		}
 
+		//If n is even, recursively call the same method with doubled x, and halved n.
+		//If n is not even, recursively call the same method with tripled x, and halved n.
 		if(n%2 ==1)
 		{
 			return x*myPow(x*x, n/2);
