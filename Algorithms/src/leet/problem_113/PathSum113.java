006aa57693cf073ba15bea688c652d0328e14e2d
Arvind <Arvind.Iyer@sephora.com>
2018-01-29 08:38:22 -0800

    Path Sum Problem

--- Algorithms/src/leet/problem_113/PathSum113.java
+++ Algorithms/src/leet/problem_113/PathSum113.java
@@ -1,5 +1,9 @@
 package leet.problem_113;
 
+import leet.node.TreeNode;
+
+//https://leetcode.com/problems/path-sum/description/
+
 /**
  *
  * @author akrishnaniyer
@@ -12,6 +16,25 @@ public class PathSum113 {
 	 */
 	public static void main(String[] args) {
 		// TODO code application logic here
+	}
+
+	/**
+	 * A straightforward tree traversal flow, attempting to find the leaf node,
+	 * which adds up to the target sum, from the root node.
+	 * @param root
+	 * @param sum
+	 * @return
+	 */
+	public boolean hasPathSum(TreeNode root, int sum) {
+
+		if (root == null)
+			return false;
+
+		if (root.left == null && root.right == null && root.val - sum == 0)
+			return true;
+
+		return (hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val));
+
 	}
 
 }
