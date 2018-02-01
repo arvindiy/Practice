f470e7b9d54d3bd848b953acfc8c59dd6d0914a4
Arvind <Arvind.Iyer@sephora.com>
2018-01-31 19:58:21 -0800

    Added some matrix algorithms

---
+++ Algorithms/src/leet/medium/problem_48/RotateImage.java
@@ -0,0 +1,41 @@
+package leet.medium.problem_48;
+
+import java.util.Arrays;
+
+public class RotateImage {
+
+	public static void main(String[] args) {
+		int[][] matrix = { { 5, 1, 9, 11 }, { 2, 4, 8, 10 }, { 13, 3, 6, 7 }, { 15, 14, 12, 16 } };
+		print(matrix);
+		new RotateImage().rotate(matrix);
+		System.out.println();
+		print(matrix);
+
+	}
+
+	private static void print(int[][] matrix) {
+		for (int[] row : matrix) {
+			System.out.println(Arrays.toString(row));
+		}
+	}
+
+	public void rotate(int[][] matrix) {
+		int start = 0;
+		int end = matrix.length-1;
+		while (start <= end) {
+			int[] tmp = matrix[start];
+			matrix[start] = matrix[end];
+			matrix[end] = tmp;
+			end--;
+			start++;
+		}
+		
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = i; j < matrix[0].length; j++) {
+				int tmp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = tmp;
+			}
+		}
+	}
+}
--- Algorithms/src/leet/medium/problem_54/SpiralMatrix.java
+++ Algorithms/src/leet/medium/problem_54/SpiralMatrix.java
@@ -1,4 +1,4 @@
-package leet;
+package leet.medium.problem_54;
 
 import java.util.ArrayList;
 import java.util.List;
