9cd7764e9d5bd5e66735c1366973c5a75f91e0d7
Arvind <Arvind.Iyer@sephora.com>
2018-01-31 22:03:35 -0800

    Find duplicate numbers

---
+++ Algorithms/src/leet/easy/problem_448/Find_All_Numbers_Disappeared_In_An_Array.java
@@ -0,0 +1,30 @@
+package leet.easy.problem_448;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+//https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/description/
+public class Find_All_Numbers_Disappeared_In_An_Array {
+
+	public static void main(String[] args) {
+		int[] arr = { 4, 3, 2, 7, 8, 2, 3, 1 };
+		System.out.println(new Find_All_Numbers_Disappeared_In_An_Array().findDisappearedNumbers(arr));
+	}
+
+	public List<Integer> findDisappearedNumbers(int[] nums) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int i = 0; i < nums.length; i++) {
+			int val = Math.abs(nums[i]) - 1;
+			if (nums[val] > 0)
+				nums[val] = -nums[val];
+		}
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] > 0)
+				result.add(i + 1);
+		}
+		System.out.println(Arrays.toString(nums));
+		return result;
+	}
+
+}
