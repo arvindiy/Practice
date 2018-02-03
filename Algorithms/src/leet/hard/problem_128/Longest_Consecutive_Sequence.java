7d7789170b1061c0504e659dd0ed88eccae40d2e
Arvind <Arvind.Iyer@sephora.com>
2018-02-02 16:42:40 -0800

    New algorithms

---
+++ Algorithms/src/leet/hard/problem_128/Longest_Consecutive_Sequence.java
@@ -0,0 +1,32 @@
+package leet.hard.problem_128;
+
+import java.util.HashMap;
+
+//https://leetcode.com/problems/longest-consecutive-sequence/description/
+
+public class Longest_Consecutive_Sequence {
+
+	public static void main(String[] args) {
+		//int[] nums = { 100, 4, 200, 1, 3, 2 };
+		int[] nums = {0,0,0,0};
+		 //int[] nums = {100, 4, 200, 1, 3, 3, 2};
+		System.out.println(new Longest_Consecutive_Sequence().longestConsecutive(nums));
+	}
+
+	public int longestConsecutive(int[] nums) {
+		int length = 0;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int num : nums) {
+			if (!map.containsKey(num)) {
+				int leftElement = map.get(num - 1) == null ? 0 : map.get(num - 1);
+				int rightElement = map.get(num + 1) == null ? 0 : map.get(num + 1);
+				int tmpMaxSum = leftElement + rightElement + 1;
+				length = Math.max(length, tmpMaxSum);
+				map.put(num, length);
+				map.put(num - leftElement, length);
+				map.put(num + rightElement, length);
+			}
+		}
+		return length;
+	}
+}
--- Algorithms/src/leet/problem_273/IntegerToEnglishWords.java
+++ Algorithms/src/leet/problem_273/IntegerToEnglishWords.java
@@ -2,43 +2,47 @@ package leet.problem_273;
 
 public class IntegerToEnglishWords {
 
-	final String[] THOUSANDS = { "", "THOUSAND", "MILLION", "BILLION" };
-	final String[] TENS = { "", "TEN", "TWENTY", "THIRTY", "FORTY", "FIFTY", "SIXTY", "SEVENTY", "EIGHTY", "NINETY" };
-	final String[] LESSTHAN20 = { "", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN",
-			"ELEVEN", "TWELVE", "THIRTEEN", "FOURTEEN", "FIFTEEN", "SIXTEEN", "SEVENTEEN", "EIGHTEEN", "NINETEEN",
-			"TWENTY" };
+	final String[] THOUSANDS = { "", "Thousand ", "Million ", "Billion " };
+	final String[] TENS = { "", "Ten ", "Twenty ", "Thirty ", "Forty ", "Fifty ", "Sixty ", "Seventy ", "Eighty ",
+			"Ninety " };
+	final String[] LESSTHAN20 = { "", "One ", "Two ", "Three ", "Four ", "Five ", "Six ", "Seven ", "Eight ", "Nine ",
+			"Ten ", "Eleven ", "Twelve ", "Thirteen ", "Fourteen ", "Fifteen ", "Sixteen ", "Seventeen ", "Eighteen ",
+			"Nineteen ", "Twenty " };
 
 	public static void main(String[] args) {
 		// int num = 673;
-		int num = 990891111;
+		int num = 1000000;
 		// int num = 2147483633;
 		System.out.println(new IntegerToEnglishWords().numberToWords(num));
 	}
 
 	public String numberToWords(int num) {
+		if (num == 0)
+			return "Zero";
 		int thousandsIndex = 0;
 		// StringBuilder result = new StringBuilder();
 		String result = "";
 		while (num > 0) {
 			String firstThree = convertThreeDigits(num % 1000);
 			// result.append(result).append(THOUSANDS[thousandsIndex]).append(firstThree);
-			result = firstThree + THOUSANDS[thousandsIndex] + " " + result;
+			if (num % 1000 != 0)
+				result = firstThree + THOUSANDS[thousandsIndex] + result;
 			thousandsIndex++;
 			num = num / 1000;
 		}
-		return result;
+		return result.trim();
 	}
 
 	public String convertThreeDigits(int num) {
 		StringBuilder result = new StringBuilder();
 		if (num < 10) {
-			result.append(LESSTHAN20[num]).append(" ");
+			result.append(LESSTHAN20[num]);
 		} else if (num < 20) {
-			result.append(LESSTHAN20[num]).append(" ");
+			result.append(LESSTHAN20[num]);
 		} else if (num < 100) {
-			result.append(TENS[num / 10]).append(" ").append(convertThreeDigits(num % 10));
+			result.append(TENS[num / 10]).append(convertThreeDigits(num % 10));
 		} else {
-			result.append(LESSTHAN20[num / 100]).append(" HUNDRED ").append(convertThreeDigits(num % 100));
+			result.append(LESSTHAN20[num / 100]).append("Hundred ").append(convertThreeDigits(num % 100));
 		}
 		return result.toString();
 	}
